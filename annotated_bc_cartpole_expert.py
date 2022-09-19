fc75b4ea36b05c092f915362e4d8c61c85a0911a
carla-garcia-medina <carla.garia.medina@outlook.com>
2022-09-19 17:37:05 -0400

    separate training expert process

---
+++ annotated_bc_cartpole_expert.py
@@ -0,0 +1,32 @@
+'''
+Imports
+'''
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import gnwrapper
+
+import gym
+from gym.wrappers import Monitor
+
+import tensorflow_hub as hub
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines import PPO1
+
+from stable_baselines.gail import generate_expert_traj
+from functools import partial
+import h5py
+import os
+from torch.utils.tensorboard import SummaryWriter
+
+
+env = gym.make('CartPole-v1')
+
+ppo1_expert = PPO1(MlpPolicy, env, verbose=0)
+ppo1_expert.learn(total_timesteps=250000)
+ppo1_expert.save("ppo1_cartpole")
+ppo1_expert = PPO1.load("ppo1_cartpole")
